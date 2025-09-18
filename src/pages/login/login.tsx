cc3012c5dfcae9a98224aacb9788738a6d102371
pablo braga <pablo.gerheim@onecontrol.io>
2025-09-17 22:07:01 -0300

    remuve-button

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "@/context/authContext";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import AnimatedGradientText from "@/components/magicui/animated-gradient-text";
 import ShimmerButton from "@/components/magicui/shimmer-button";
